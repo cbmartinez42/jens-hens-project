9f59683a25d14e8bca0235eb574ea92629d59276
Mark Artim <mark.artim@gmail.com>
2021-05-13 21:47:16 -0500

    order api's for get all, get my orders and get order by id

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,26 +1,45 @@
 // const { Order } = require("../../models");
 
-var checkOutBtn = false;
-console.log(checkOutBtn);
+// var checkOutBtn = false;
+// console.log(checkOutBtn);
 
-router.get('/order/:id', async (req, res) => {
-    try {
-        checkOutBtn = true;
-        console.log('checkout clicked');
-        console.log(checkOutBtn);
-        const orderData = await Order.findByPk(req.params.id);
-        const order = orderData.get({ plain: true });
-        res.render('checkout', {
-        ...order,
-        checkOutBtn: checkOutBtn,
-        // logged_in: req.session.logged_in,
-        // userName: req.session.userName,
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+const getOrder = async () => {
+    // event.preventDefault();
+    const url = window.location.pathname;
+    const id = url.substring(url.lastIndexOf('/') + 1)
+    const response = await fetch(`/api/order/${id}`, {
+      method: 'GET',
     });
+  
+    if (response.ok) {
+        // return response;
+        document.location.replace('/checkout');
+    } else {
+      alert(response.statusText);
+    }
+  };
+  
+  document.querySelector('#checkOutBtn').addEventListener('click', getOrder);
+
+
+// router.get('/order/:id', async (req, res) => {
+//     try {
+//         checkOutBtn = true;
+//         console.log('checkout clicked');
+//         console.log(checkOutBtn);
+//         const orderData = await Order.findByPk(req.params.id);
+//         const order = orderData.get({ plain: true });
+//         res.render('checkout', {
+//         ...order,
+//         checkOutBtn: checkOutBtn,
+//         // logged_in: req.session.logged_in,
+//         // userName: req.session.userName,
+//         });
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+//     });
     
-document
-.querySelector('#checkout')
-.addEventListener('click', btnHandler);
+// document
+// .querySelector('#checkout')
+// .addEventListener('click', btnHandler);
