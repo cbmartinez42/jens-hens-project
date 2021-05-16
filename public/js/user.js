a2ee81b8e2153e63b8965d58f0930542c2a4c1fe
Mark Artim <mark.artim@gmail.com>
2021-05-16 16:08:20 -0500

    Grant admin priveledges in user screen now working

--- public/js/user.js
+++ public/js/user.js
@@ -1,8 +1,8 @@
-
-
-
 const adminUpdate = async (event) => {
+    console.log('userBody was clicked!');
+    console.log('the event is as folows:',event);
     const id = event.target.getAttribute('data-id');
+    console.log('and the id is ',id);
 
     const response = await fetch(`/api/user/${id}`, {
         method: 'PUT',
@@ -12,9 +12,8 @@ const adminUpdate = async (event) => {
         }),
         headers: { 'Content-Type': 'application/json' },
         });
+    location.reload();
 
 }
 
 document.querySelector('.userBody').addEventListener("click",adminUpdate)
-
-
