100485339c8b5c2b3d8c09a486c7983f6c5081ba
Jay Yousef <jay.yousef@gmail.com>
2021-05-16 14:27:18 -0500

    condensed the Users page and the Orders page to the dashboard page only viewable by admins. Users wil see other information based on their status and recent orders, though the page does need to be styled

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const User = require('../../models/User');
 
 
-
 router.post('/', async (req, res) => {
     try {
         console.log(req.body);
@@ -22,22 +21,33 @@ router.post('/', async (req, res) => {
         });
     } catch (err) {
         console.log(err)
-        res.status(400).json({message: "Don't panic. But one of those was wrong. Please try again", err})
+        res.status(400).json({
+            message: "Don't panic. But one of those was wrong. Please try again",
+            err
+        })
     }
 });
 
 router.post('/login', async (req, res) => {
     try {
-        const userData = await User.findOne({ where: { email: req.body.email } });
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
         if (!userData) {
-            res.status(400).json({ message: "This is not looking good...you'd better try again"});
+            res.status(400).json({
+                message: "This is not looking good...you'd better try again"
+            });
             return;
         }
 
         const validPassword = await userData.checkPassword(req.body.password);
 
         if (!validPassword) {
-            res.status(400).json({ message: "This is not looking good...you'd better try again"});
+            res.status(400).json({
+                message: "This is not looking good...you'd better try again"
+            });
             return;
         }
 
@@ -45,7 +55,10 @@ router.post('/login', async (req, res) => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
 
-            res.json({ user: userData, message: "Welcome! You did it!"});
+            res.json({
+                user: userData,
+                message: "Welcome! You did it!"
+            });
         });
     } catch (err) {
         res.status(400).json(err);
@@ -54,13 +67,60 @@ router.post('/login', async (req, res) => {
 
 router.post('/logout', (req, res) => {
     if (req.session.logged_in) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
     } else {
-      res.status(404).end();
+        res.status(404).end();
     }
-  });
-  
-  
+});
+
+router.get('/:id', async (req, res) => {
+    const id=req.params.id;
+    try {
+        const userData = await User.findOne({
+            where: {
+                id: `${id}`
+            }
+        });
+        if (!userData) {
+            res.status(400).json({
+                message: "This is not looking good...you'd better try again"
+            });
+            return;
+        }
+    }
+    catch (err) {
+        console.log(err)
+        res.status(400).json({
+            message: "Don't panic. But one of those was wrong. Please try again",
+            err
+        })
+    }
+});
+
+router.put('/:id', async (req, res) => { //withAuth, 
+    // const id = req.params.id
+    try {
+        const userData = await User.update({
+            admin: req.body.admin,
+            request_admin: req.body.request_admin
+        }, {
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!userData) {
+            res.status(404).json({
+                message: 'No user with this ID. Please try again.'
+            });
+            return;
+        }
+        res.status(200).json(userData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+
 module.exports = router;
