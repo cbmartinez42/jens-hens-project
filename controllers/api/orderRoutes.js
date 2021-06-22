3aeb3d1044b9f6e4ca21a1c413a5a5788c2d2947
cbmartinez42 <cbmartinez42@gmail.com>
2021-06-22 18:34:04 -0500

    adding some sendgrid function to orderRoutes in api controllers

--- controllers/api/orderRoutes.js
+++ controllers/api/orderRoutes.js
@@ -1,6 +1,43 @@
 const router = require('express').Router();
 const { Order, User } = require('../../models');
 // const withAuth = require('../../utils/auth');
+const fetch = require("node-fetch");
+// mail handler
+
+const mailHandler = (orderData, email) => {
+  
+  console.log('handler fired', orderData)
+  console.log('handler fired email ', email)
+  const total = orderData.order_quantity * .50
+  console.log('total',  total)
+  const fetchRequest = (("https://api.sendgrid.com/v3/mail/send", {
+    body: `{\n   'from':{\n      'email':'jenns.hens.eggs@gmail.com'\n   },\n   'personalizations':[\n      {\n         'to':[\n            {\n               'email': ${email}\n            }\n         ],\n         'dynamic_template_data':{\n            'total':'$' + ${total},\n            'items':[\n               {\n                  'text':'Farm Fresh Eggs',\n               },\n\n            ],\n            'receipt':true,\n          }\n      }\n   ],\n   'template_id':'${process.env.SG_TEMPLATE}'\n}`,
+    headers: {
+      "Authorization": `Bearer ${process.env.SG_KEY}`,
+      "Content-Type": "application/json"
+    },
+    method: "POST"
+  }))
+
+  console.log('fetch request ... ', fetchRequest)
+  
+  fetch("https://api.sendgrid.com/v3/mail/send", {
+    body: `{\n   'from':{\n      'email':'jenns.hens.eggs@gmail.com'\n   },\n   'personalizations':[\n      {\n         'to':[\n            {\n               'email': ${email}\n            }\n         ],\n         'dynamic_template_data':{\n            'total':'$' + ${total},\n            'items':[\n               {\n                  'text':'Farm Fresh Eggs',\n               },\n\n            ],\n            'receipt':true,\n          }\n      }\n   ],\n   'template_id':'${process.env.SG_TEMPLATE}'\n}`,
+    headers: {
+      "Authorization": `Bearer ${process.env.SG_KEY}`,
+      "Content-Type": "application/json"
+    },
+    method: "POST"
+  })
+    .then (function (response) {
+      console.log('handler end >> ', response)
+    }
+
+    )
+    .catch(function (error) {
+      console.log(error)
+    })
+  }
 
 //GET ALL ORDERS
 router.get('/', async (req, res) => {
@@ -86,19 +123,30 @@ router.get('/myorders', async (req, res) => {
   });
   
   router.post('/', async(req,res) => {
-    console.log(req.body)
     const order_quantity = req.body.qty;
     const special_instructions = req.body.special_instructions;
     const customer = req.session.user_id;
+    const userData = await User.findOne({
+      where: {
+          id: customer,
+      }
+    })
+    const email = userData.email
+    // console.log("email >>>>", email)
+    // console.log("customer  >>", customer)
     const orderData = await Order.create({order_quantity:order_quantity, customer:customer, spec_inst: special_instructions});
+    
     if(orderData){
+      mailHandler(orderData, email)
       res.status(200).json(orderData)
+
     }
    else{
      res.status(400).json(err)
    }
   })
 module.exports = router;
+
 
 //UPDATE ORDER FULFILLED FLAG ONLY
 router.put('/:id', async (req, res) => { //withAuth, 
--- controllers/index.js
+++ controllers/index.js
@@ -2,31 +2,31 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const htmlRoutes = require('./html');
-const sgMail = require('@sendgrid/mail')
+// const sgMail = require('@sendgrid/mail')
 
 router.use('/', htmlRoutes);
 router.use('/api', apiRoutes);
 
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-const msg = {
-  to: 'cbmartinez42@gmail.com', // Change to your recipient
-  from: 'jenns.hens.eggs@gmail.com', // Change to your verified sender
-  subject: 'Thank you for your order!',
-//   text: 'We will be in contact with you within 72 hours',
-  html: 'We will be in contact with you within 72 hours at the phone number associated with your account!',
-}
+// const msg = {
+//   to: 'cbmartinez42@gmail.com', // Change to your recipient
+//   from: 'jenns.hens.eggs@gmail.com', // Change to your verified sender
+//   subject: 'Thank you for your order!',
+// //   text: 'We will be in contact with you within 72 hours',
+//   html: 'We will be in contact with you within 72 hours at the phone number associated with your account!',
+// }
 
-sgMail
-  .send(msg)
-  .then((response) => {
-    console.log(response[0].statusCode)
-    console.log(response[0].headers)
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+// sgMail
+//   .send(msg)
+//   .then((response) => {
+//     console.log(response[0].statusCode)
+//     console.log(response[0].headers)
+//   })
+//   .catch((error) => {
+//     console.error(error)
+//   })
 
 
 module.exports = router;
